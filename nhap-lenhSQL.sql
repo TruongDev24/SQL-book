f29f8fcba4480f9f44162a01daeaf9e2f8e6b8be
TruongDev24 <151207446+TruongDev24@users.noreply.github.com>
2024-03-10 00:55:47 +0700

    sql mới

--- nhap-lenhSQL.sql
+++
@@ -1,5 +0,0 @@
-select S.vi_tri,
-		concat(DM.vi_tri, TL.vi_tri, S.vi_tri) as vitrisach
-from Sach S
-inner join TheLoai TL on TL.id = S.the_loai
-inner join DanhMuc DM on DM.id = TL.danh_muc
--- selectAll.sql
+++ selectAll.sql
@@ -1,39 +1,9 @@
-﻿-- Chọn tất cả dữ liệu từ tất cả các bảng
-SELECT * FROM dbo.Account;
-SELECT * FROM dbo.DanhMuc;
+SELECT * FROM dbo.KhuyenMai;
+SELECT * FROM dbo.KhachHang;
 SELECT * FROM dbo.TheLoai;
-SELECT * FROM dbo.SinhVien;
-SELECT * FROM dbo.PhieuMuon;
 SELECT * FROM dbo.Sach;
-SELECT * FROM dbo.ChiTietPhieuMuon;
-
-
--- Tắt kiểm tra khóa ngoại để có thể xóa dữ liệu từ các bảng có liên kết
-EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'
-
--- Xóa dữ liệu từ tất cả các bảng
-EXEC sp_MSForEachTable 'DELETE FROM ?'
-
--- Đặt lại giá trị identity cho bảng dbo.Account
-DBCC CHECKIDENT ('dbo.Account', RESEED, 0);
-
--- Đặt lại giá trị identity cho bảng dbo.DanhMuc
-DBCC CHECKIDENT ('dbo.DanhMuc', RESEED, 0);
-
--- Đặt lại giá trị identity cho bảng dbo.TheLoai
-DBCC CHECKIDENT ('dbo.TheLoai', RESEED, 0);
-
--- Đặt lại giá trị identity cho bảng dbo.SinhVien
-DBCC CHECKIDENT ('dbo.SinhVien', RESEED, 0);
-
--- Đặt lại giá trị identity cho bảng dbo.PhieuMuon
-DBCC CHECKIDENT ('dbo.PhieuMuon', RESEED, 0);
-
--- Đặt lại giá trị identity cho bảng dbo.Sach
-DBCC CHECKIDENT ('dbo.Sach', RESEED, 0);
-
--- Đặt lại giá trị identity cho bảng dbo.ChiTietPhieuMuon
-DBCC CHECKIDENT ('dbo.ChiTietPhieuMuon', RESEED, 0);
-
--- Bật lại kiểm tra khóa ngoại
-EXEC sp_MSForEachTable 'ALTER TABLE ? CHECK CONSTRAINT ALL'
+SELECT * FROM dbo.LichSuNhapHang;
+SELECT * FROM dbo.ChiTietKhuyenMai;
+SELECT * FROM dbo.Account;
+SELECT * FROM dbo.HoaDon;
+SELECT * FROM dbo.ChiTietHoaDon;
